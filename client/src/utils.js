3d57ff614cdf3f2a59a57e8e18fdaace0d676de8
dsdunn <dsdunnmusic@gmail.com>
2020-05-16 20:36:53 -0600

    Handle Genres

--- client/src/utils.js
+++ client/src/utils.js
@@ -2,4 +2,31 @@ export const filter = (restaurants, criteria = {}) => {
   return restaurants.sort((jointA, jointB) => {
     return jointA.name > jointB.name ? 1 : -1;
   })
+};
+
+export const getGenres = (restaurants = []) => {
+  let genres = [];
+  let genreReducer = (genreList) => {
+    genreList.forEach(genre => {
+      if (!genres.includes(genre)){
+        genres.push(genre);
+      }
+    })
+  }
+
+  restaurants.forEach(joint => {
+    let { genre } = joint;
+
+    if (genre) {
+      let genreList = genre.split(',');
+
+      genreReducer(genreList);
+
+      joint.genre = genreList.join(', ');
+    }
+  })
+  return ({
+    formattedRestaurants: restaurants,
+    genres
+  });
 }
