13d4f7e37795b6159c61b79ea77ae79f38218688
dsdunn <dsdunnmusic@gmail.com>
2020-09-29 22:14:43 -0600

    Updates

---
+++ precache-manifest.2845eb7f484aebfcd1fc72afcb35579a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95b4ff82f383862bb3b10c4b41afae9",
+    "url": "/restaurants/index.html"
+  },
+  {
+    "revision": "d2699f1527966e8b89de",
+    "url": "/restaurants/static/css/main.18ce76a5.chunk.css"
+  },
+  {
+    "revision": "b5fcd2ded1a584a42169",
+    "url": "/restaurants/static/js/2.da253410.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/restaurants/static/js/2.da253410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2699f1527966e8b89de",
+    "url": "/restaurants/static/js/main.b86acaef.chunk.js"
+  },
+  {
+    "revision": "561c4ac3cf04aa5ab4ba",
+    "url": "/restaurants/static/js/runtime-main.6dde5bd6.js"
+  }
+]);
