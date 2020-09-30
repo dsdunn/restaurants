717297b5d7d58b6ad81c456a9fafeb0a82ecd1af
dsdunn (via Travis CI) <dsdunnmusic@gmail.com>
2020-09-30 05:06:24 +0000

    Deploy dsdunn/restaurants to github.com/dsdunn/restaurants.git:gh-pages

---
+++ precache-manifest.551bb74fe8a2e4e74f3e032fc1dd1034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4728d83375fa75e2ca4603f4d7b4e740",
+    "url": "/restaurants/index.html"
+  },
+  {
+    "revision": "2c91498d431f5da9670b",
+    "url": "/restaurants/static/css/main.18ce76a5.chunk.css"
+  },
+  {
+    "revision": "97224f5a933e6e242c9a",
+    "url": "/restaurants/static/js/2.246b55b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/restaurants/static/js/2.246b55b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c91498d431f5da9670b",
+    "url": "/restaurants/static/js/main.9738c577.chunk.js"
+  },
+  {
+    "revision": "561c4ac3cf04aa5ab4ba",
+    "url": "/restaurants/static/js/runtime-main.6dde5bd6.js"
+  }
+]);
